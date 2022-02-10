a92e7a01b868edf6da4b7e894d6d7a389b3c1e4a
ÐFarma20 <kustov.ius@students.dvfu.ru>
2022-02-10 17:55:19 +1000

    Добален класс комнаты с реализованным спавном врагов в нутри комнаты

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,12 +7,13 @@
 
 using namespace std;
 
-pair <int, int> WorldSize{ 20, 20 };
+pair <int, int> WorldSize{ 25, 25 };
 
 class Character {
 protected:
 	int heals;
 	int damage;
+	int LVL;
 	char name;
 	pair<int, int> position, oldPosition;
 public:
@@ -46,7 +47,7 @@ private:
 
 public:
 	Hero() {
-		heals = 100, damage = 10,  name = 'A', position = { 0, 0 };
+		heals = 100, damage = 10, LVL = 1, name = 'A', position = { 0, 0 };
 	};
 
 	void SetPosition(char _button) override {
@@ -79,22 +80,56 @@ public:
 
 class Zombi : public Character {
 public:
-	Zombi() {
-		heals = 20, damage = 5, name = 'Z', oldPosition = position = { rand() % WorldSize.first, rand() % WorldSize.second};
+	Zombi(pair<int, int> _position) {
+		heals = 20, damage = 5, LVL = 1, name = 'Z', oldPosition = position = _position;
 	}
 
 	void SetPosition(char _button) override {
 		if (_button == 'w' || _button == 's' || _button == 'a' || _button == 'd') {
-			int rand_x = rand() % 3 - 1;
 			int rand_y = rand() % 3 - 1;
-			if ((position.first + rand_x < WorldSize.first) && (position.first + rand_x >= 0) &&
-				(position.second + rand_y < WorldSize.second) && (position.second + rand_y >= 0)) {
+			int rand_x = rand() % 3 - 1;
+			if ((position.first + rand_y < WorldSize.second) && (position.first + rand_y >= 0) &&
+				(position.second + rand_x < WorldSize.first) && (position.second + rand_x >= 0)) {
 				
 				oldPosition = position;
-				position = { position.first + rand_x, position.second + rand_y };
+				position = { position.first + rand_y, position.second + rand_x };
 			}
 		}
 		
+	}
+};
+
+class Room {
+protected:
+	int enemyCount;
+	int enemyMaxLVL;
+	int itemsCount;
+	char wall;
+	pair<int, int>roomPointFirst;
+	pair<int, int>roomPointSecond;
+public:
+	pair<int, int>GetRoomPointFirst(){ return roomPointFirst; }
+	pair<int, int>GetRoomPointSecond(){ return roomPointSecond; }
+	char GetWall() { return wall; }
+
+	vector<Character*> SpawnEnemys() {
+		vector<Character*>Enemys(enemyCount);
+		for (int i = 0; i < enemyCount; i++) {
+			pair<int, int>positionInRoom {
+				(roomPointFirst.first + 1) + rand() % (roomPointSecond.first - (roomPointFirst.first + 2)),
+				(roomPointFirst.second + 1) + rand() % (roomPointSecond.second - (roomPointFirst.second + 2))
+			};
+			Enemys[i] = new Zombi(positionInRoom);
+
+		}
+		return Enemys;
+	}
+};
+
+class EzRoom : public Room {
+public:
+	EzRoom() {
+		enemyCount = 3, enemyMaxLVL = 1, itemsCount = 0, wall = '#', roomPointFirst = { 8, 10 }, roomPointSecond = { 16, 16 };
 	}
 };
 
@@ -106,7 +141,7 @@ private:
 	char floor;
 	vector<vector<char>> location;
 public:
-	Map() : location(vector<vector<char>>(width, vector<char>(height, '_'))), width(WorldSize.first), height(WorldSize.second), floor('_') {}
+	Map() : location(vector<vector<char>>(width, vector<char>(height, '_'))), width(WorldSize.second), height(WorldSize.first), floor('_') {}
 
 	void WriteMap(){
 		system("cls");
@@ -125,16 +160,29 @@ public:
 			location[Obj->GetPosition().first][Obj->GetPosition().second] = Obj->GetName();
 		}
 	}
+
+	void SetPosition(Room* room) {
+		for (int i = room->GetRoomPointFirst().first; i <= room->GetRoomPointSecond().first;  i++) {
+			for (int j = room->GetRoomPointFirst().second; j <= room->GetRoomPointSecond().second; j++) {
+				if (i == room->GetRoomPointFirst().first ||
+					i == room->GetRoomPointSecond().first ||
+					j == room->GetRoomPointFirst().second ||
+					j == room->GetRoomPointSecond().second) {
+
+					location[i][j] = room->GetWall();
+				}
+				else { location[i][j] = '*'; }
+			}
+		}
+	}
 };
 
 void GamePlay() {
 	Map map;
+	EzRoom room;
+	map.SetPosition(&room);
+	vector<Character*>gameObjects = room.SpawnEnemys();
 	Character* hero = new Hero;
-	Zombi zombi;
-	vector<Character*>gameObjects(10);
-	for (int i = 0; i < 10; i++) {
-		gameObjects[i] = new Zombi;
-	}
 	gameObjects.push_back(hero);
 	char moveBatton;
 	while (1) {
